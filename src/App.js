9b88f5c4bcd0412ccd1311bd95cb65e9026533bc
Ravi Kumar Pal <ravikumarpal906@gmail.com>
2022-12-16 19:57:06 +0530

    nn

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
 
 function App() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <TextForm />
+    </>
+    
   );
 }
 
---
+++ src/components/TextForm.js
@@ -0,0 +1,38 @@
+import React, {useState} from 'react';
+
+
+export default function TextForm(props) {
+    const handleUpClick =()=>{
+        let newText=text.toUpperCase();
+        setText(newText);
+    }
+
+    const handleloClick =()=>{
+        let newText=text.toLowerCase();
+        setText(newText);
+    }
+
+    const handleonChange=(event)=>{
+        setText(event.target.value);
+    }
+
+    const[text,setText] = useState();
+  return (
+    <>
+    <div className='textForm'>
+        <h2>{props.heading}</h2>
+        <div className="my-3">
+            <textarea className='from-control' value={text} onChange={handleonChange} id="myBox" row="10" ></textarea>
+        </div>
+        <button className='btn btn-primary mx-3' onClick={handleUpClick}>convert to uppercase</button>
+        <button className='btn btn-primary mx-3' onClick={handleloClick}>convert to lowercase</button>
+    </div>
+    <div className='container my-3'>
+        <h2>Text summary</h2>
+        <p>{text.split(" ").length} word and {text.length} characters</p>
+        <h2>Preview</h2>
+        <p>{text}</p>
+    </div>
+    </>
+  )
+}
